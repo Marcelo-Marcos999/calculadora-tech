5aaf60297a4634bb53107acbae5f3e2ecb7d492c
root <root@srv-d8qmhqa8qa3s73dm0esg-hibernate-74cdd44fc8-l5xkr>
2026-06-19 19:19:57 +0000

    feat: add src/components/calculatorDisplay.js

---
+++ src/components/calculatorDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalculatorDisplay = ({ value }) => {
+  return (
+    <div className="calculator-display">
+      <p>{value}</p>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
